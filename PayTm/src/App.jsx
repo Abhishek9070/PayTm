512651cd83ddf0b036bb8619892a53f1e45a795c
Abhishek9070 <abhishekmishra051028@gmail.com>
2026-04-19 11:09:01 +0530

    setting up project

---
+++ PayTm/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-blue-600 bg-red-500 text-2xl border-l-amber-300">PayTm clone</h1>
+    </div>
+  )
+}
+
+export default App
